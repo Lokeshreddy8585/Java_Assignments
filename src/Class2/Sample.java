93b5e72a4f674b3b86024a0f1a09547b5f740821
Lokeshreddy <lokeshreddy8585@gmail.com>
2024-03-01 09:58:30 -0600

    1st commit

---
+++ src/Class2/Sample.java
@@ -0,0 +1,12 @@
+package Class2;
+
+public class Sample {
+
+    public static void main(String args[])
+    {
+        int x = 3;
+
+        if(x==3)
+            System.out.println("x is equal to 3");
+    }
+}
