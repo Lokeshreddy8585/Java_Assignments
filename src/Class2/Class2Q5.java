dc86bfb20e81a9620e2bbcaba8a46805300cca72
Lokeshreddy <lokeshreddy8585@gmail.com>
2024-03-01 12:03:29 -0600

    commit

---
+++ src/Class2/Class2Q5.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class Class2Q5 {
+    public static void main(String args[])
+    {
+        char x = '4',y = '5';
+
+        if(x+y==9){
+            System.out.println("Data type of x and y is integer");
+        }
+        else{
+            System.out.println("Data type of x and y is character");
+        }
+    }
+}
